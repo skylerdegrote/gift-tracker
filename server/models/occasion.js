d1953906fe0f19e090e57a0bc0de0f68510309b1
[MStommes] <mstommes@gmail.com>
2015-10-26 14:52:59 -0500

    created holidays schema

---
+++ server/models/occasion.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose'),
+    occasions = require('./occasions'),
+    Schema = mongoose.Schema;
+
+var OccasionsSchema = new Schema({
+    name: String,
+    date: Date,
+    gifts: [{
+        name: String,
+        stores:[],
+        imageUrl: {type: String, default: 'assets/images/gifts.jpg'},
+        purchased: Boolean,
+        notes: String
+    }]
+});
+
+var occasion = mongoose.model('occasions', OccasionsSchema);
+module.exports = occasion;
