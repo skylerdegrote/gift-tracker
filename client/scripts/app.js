665d04f8d81f5eb47a05fdda19df462969c260a3
Skyler <skyler.m.degrote@outlook.com>
2015-10-26 13:03:06 -0500

    angular materials set up

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,5 +3,15 @@
  */
     // Include app dependency on ngMaterial
 
-angular.module( 'YourApp', [ 'ngMaterial' ] )
-    .controller("YourController", YourController );
+//angular.module( 'YourApp', [ 'ngMaterial' ] )
+//    .controller("YourController", YourController );
+
+
+
+var app = angular.module('StarterApp', ['ngMaterial']);
+
+app.controller('AppController', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
+    $scope.toggleSidenav = function(menuId) {
+        $mdSidenav(menuId).toggle();
+    };
+}]);
