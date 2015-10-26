0acc560e6df3899bbf878f707f69e396a126b0a9
Skyler <skyler.m.degrote@outlook.com>
2015-10-26 10:55:26 -0500

    add to readme

---
+++ client/scripts/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Skyler DeGrote on 10/26/15.
+ */
+    // Include app dependency on ngMaterial
+
+angular.module( 'YourApp', [ 'ngMaterial' ] )
+    .controller("YourController", YourController );
