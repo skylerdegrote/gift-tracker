87f5ab08deaf76760b0a9e6700de4737ae44a4d1
Skyler <skyler.m.degrote@outlook.com>
2015-10-31 13:10:57 -0500

    edited the user.js schema

--- server/models/user.js
+++ server/models/user.js
@@ -2,14 +2,26 @@ var mongoose = require('mongoose');
 var user = require('./users');
 var Schema = mongoose.Schema;
 var UserSchema = new Schema({
-    firstName: String,
+    firstName: {
+        type: String,
+        required: true
+    },
     lastName: String,
     gender: String,
     email: String,
     birthDate: Date,
-    imageUrl: {type: String, default: 'assets/images/user.jpg'},
-    userName: String,
-    password: String,
+    imageUrl: {
+        type: String,
+        default: 'assets/images/user.jpg'
+    },
+    userName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
     occasions:[{
         name: String,
         date: Date,
@@ -22,47 +34,19 @@ var UserSchema = new Schema({
         email: String,
         birthDate: Date,
         imageUrl: {type: String, default: 'assets/images/user.jpg'},
-        gifts: [{
-            name: String,
-            imageUrl: {type: String, default: 'assets/images/gifts.jpg'},
-            to: [String],
-            stores:[],
-            purchased: Boolean,
-            price: Number,
-            notes: String
-        }]
-    }],
+        notes: String
+        }],
     gifts: [{
         name: String,
         imageUrl: {type: String, default: 'assets/images/gifts.jpg'},
-        to: [],
+        to: [String],
+        description: String,
         stores:[],
         purchased: Boolean,
         price: Number,
         notes: String
     }],
     notes: String
-
-    //firstName: String,
-    //lastName: String,
-    //email: String,
-    //birthdate: Date,
-    //username: String,
-    //password: String,
-    //occasions:[],
-    //people: [],
-    //gifts: [{
-    //    name: String,
-    //    occasion:[],
-    //    stores:[],
-    //    people: [],
-    //    imageUrl: {
-    //        type: String,
-    //        default: 'assets/images/gifts.jpg'
-    //    },
-    //    purchased: Boolean,
-    //    notes: String
-    //}]
 });
 
 var User = mongoose.model('user', UserSchema);
