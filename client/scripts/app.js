2201314b27cee7cb6a62ded8fa3fe151042ec8c5
Skyler <skyler.m.degrote@outlook.com>
2015-10-31 19:59:48 -0500

    add all the views

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -8,11 +8,54 @@
 
 
 
-var app = angular.module('StarterApp', ['ngMaterial']);
+var app = angular.module('myApp', ['ngMaterial', 'ngRoute']);
 
 app.controller('AppController', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
     $scope.toggleSidenav = function(menuId) {
         $mdSidenav(menuId).toggle();
     };
+
+    $scope.toggleMenu = function(menuId){
+        header(menuId).toggle();
+    };
 }]);
 
+
+app.controller('RootCtrl', ['$scope', function($scope){
+    $scope.title = "Home Page";
+}]);
+
+app.controller('CatsCtrl', ['$scope', function($scope){
+    $scope.title = "Cats Page";
+}]);
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/home', {
+            templateUrl : './home.html'
+        })
+        .when('/person', {
+            templateUrl : './person.html'
+        })
+        .when('/occasion', {
+            templateUrl : './occasion.html'
+        })
+        .when('/login', {
+            templateUrl : './login.html'
+        })
+        .when('/register', {
+            templateUrl : './register.html'
+        })
+        .when('/newperson', {
+            templateUrl : './newperson.html'
+        })
+        .when('/newoccasion', {
+            templateUrl : './newoccasion.html'
+        })
+        .when('/newgift', {
+            templateUrl : './newgift.html'
+        })
+        .otherwise({
+            redirectTo : '/home.html'
+        });
+}]);
